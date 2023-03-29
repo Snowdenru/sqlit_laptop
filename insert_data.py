7c26e0ec6bedb7433e5f4d8d0a735752dd2e740d
Subachev Denis <subachevdenis@yandex.ru>
2023-03-29 20:26:25 +0300

    Ревью кода

--- insert_data.py
+++ insert_data.py
@@ -3,14 +3,10 @@ import sqlite3
 conn = sqlite3.connect('company.db')
 cursor = conn.cursor()
 
-
 company_list = []
 with open ("DATA/company.txt",'r') as f:
     for line in f:
-        print(1, line)
         item = line.strip().split(',')
-        print(2, item)
-        print(3,tuple([int(item[0]), item[1]]))
         company_list.append(tuple([int(item[0]), item[1]]))
 # print(company_list) # [(1, 'ASUS'), (2, 'ACER'), (3, 'AGB'), (4, 'IBALL'), (5, 'HP')]
 
